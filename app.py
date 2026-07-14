c33f4af937870a27afd25deefd757453a3eeea8d
Rudra Sharma <rud62442@gmail.com>
2026-07-14 04:12:35 +0000

    Added dummy vulnerabilities and bugs for sonarqube analysis

--- app.py
+++ app.py
@@ -18,3 +18,19 @@ def a():
 def risky_division(x, y):
     return x / y
 
+
+db_password = "admin123"
+
+import sqlite3
+def get_user(username):
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE name = '" + username + "';")  
+    return cursor.fetchall()
+
+
+def buggy_division(x, y):
+    return x / y  
+
+def print_name(user):
+    print(user["name"])  
