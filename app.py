0525e8e90101e221918792beb4145cf47c6516ee
Rudra Sharma <rud62442@gmail.com>
2026-07-14 04:03:33 +0000

    Introduced dummy issues for sonarqube analysis

--- app.py
+++ app.py
@@ -1,2 +1,20 @@
 print("Hello from Jenkins Pipeline!")
 
+
+
+unused_var = 42
+
+
+def greet():
+    print("Hello")
+
+def greet_again():
+    print("Hello")  
+
+def a():
+    pass
+
+
+def risky_division(x, y):
+    return x / y
+
